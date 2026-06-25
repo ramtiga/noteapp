51c1d7b34624e7c50990b8388780d35788b95145
ramtiga <dhanegm731@gmail.com>
2026-06-25 17:47:59 +0900

    Mainコンポーネント作成

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,10 @@ function App() {
     setNotes(filterNotes)
   }
 
+  const getActiveNote = () => {
+    return notes.find((note) => note.id === activeNote)
+  }
+
   return (
     <div className="App">
       <Sidebar
@@ -36,7 +40,7 @@ function App() {
         activeNote={activeNote}
         setActiveNote={setActiveNote}
       />
-      <Main />
+      <Main activeNote={getActiveNote()} />
     </div>
   )
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,26 @@
+import type { Note } from '../types'
 import './Main.css'
 
-const Main = () => {
-  return <div>Main</div>
+type Props = {
+  activeNote: Note | undefined
+}
+
+const Main = ({ activeNote }: Props) => {
+  if (!activeNote) {
+    return <div className="no-active-note">ノートが選択されていません</div>
+  }
+  return (
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input type="text" />
+        <textarea id="" placeholder="ノート内容を記入"></textarea>
+      </div>
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">{activeNote.title}</h1>
+        <div className="markdown-preview">{activeNote.content}</div>
+      </div>
+    </div>
+  )
 }
 
 export default Main
